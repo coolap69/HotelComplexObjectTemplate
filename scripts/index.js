709df7b844267381802b103f96213de80d0f8d6e
Louis Pazos <louispazos@gmail.com>
2017-11-21 02:34:29 +0000

    Room info, reservations, choices

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,5 @@
 var hotel = {
-    rooms: [
-        {
+    rooms: [{
             name: "Double Bed",
             price: "$75.00",
             available: 40
@@ -14,6 +13,11 @@ var hotel = {
             name: "Luxery Suite",
             price: "$250.00",
             available: 5
+        },
+        {
+            name: "Penthouse w/pool",
+            price: "$6.00",
+            available: 2
         }
     ],
     name: "CareerDevs Innt"
@@ -25,13 +29,20 @@ var hotel = {
 // console.log(document.getElementById("radio").nodeName);
 // console.log(document.getElementById("label").nodeName);
 
+function displayDetails(room) {
+    document.getElementById("price").innerHTML = hotel.rooms[room].price;
+    document.getElementById("available").innerHTML = hotel.rooms[room].available;
+}
 
 for (var i = 0; i < hotel.rooms.length; i++) {
+    console.log("start");
     var radioBtn = document.createElement("INPUT");
     radioBtn.setAttribute("type", "radio");
     radioBtn.setAttribute("name", "roomType");
     radioBtn.setAttribute("value", i);
     radioBtn.setAttribute("id", "room" + i);
+    // radioBtn.onclick = displayDetails(i)
+    radioBtn.setAttribute("onclick", "displayDetails(" + i + ")")
     var radioLbl = document.createElement("LABEL");
     radioLbl.innerHTML = hotel.rooms[i].name;
     document.getElementById("radialSection").appendChild(radioBtn);
@@ -39,4 +50,27 @@ for (var i = 0; i < hotel.rooms.length; i++) {
 
 }
 
+document.getElementById("reservationForm").onsubmit = function(event) {
+    event.preventDefault();
+    if (!document.getElementById("confirmation").checked) {
+        alert("Please agree to terms");
+        return;
+    }
 
+    var radios = document.getElementsByName("roomType");
+    var roomSelected = "";
+    for (var i = 0; i < radios.length; i++) {
+        if (!radios[i].checked) {
+            roomSelected = radios[i].value;
+            break;
+        }
+    }
+
+    if (roomSelected == "") {
+        alert("Pick a room please.")
+        return;
+    }
+    
+    alert("You have reserved room " + hotel.rooms[parseInt(roomSelected)].name + ".");
+
+}
